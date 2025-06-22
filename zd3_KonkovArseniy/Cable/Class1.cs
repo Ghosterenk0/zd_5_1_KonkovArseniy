a5238e5040d0c5389b06bfba594bd5ffe4cf5705
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-06-22 16:57:10 +0300

    f

---
+++ zd3_KonkovArseniy/Cable/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cables
+{
+    public class Cable
+    {
+        private string type;
+        private int wirecount;
+        private double diameter;
+
+        public string Type { get => type; set => type = value; }
+        public int WireCount { get => wirecount; set => wirecount = value; }
+        public double Diameter { get => diameter; set => diameter = value; }
+
+        private string manufacturer;
+        private double pricePerMeter;
+        public string Manufacturer { get=>manufacturer; set=> manufacturer = value; }
+        public double PricePerMeter { get=>pricePerMeter; set=>pricePerMeter = value; }
+
+        public Cable(string type, int wireCount, double diameter, string manufacturer, double pricePerMeter)
+        {
+            Type = type;
+            WireCount = wireCount;
+            Diameter = diameter;
+            Manufacturer = manufacturer;
+            PricePerMeter = pricePerMeter;
+        }
+        public double CalculateQuality()
+        {
+            return Diameter / WireCount;
+        }
+        public string DisplayInfo()
+        {
+            string res = "";
+
+            res += $"Тип кабеля: {Type}\n";
+            res += $"Количество жил: {WireCount}\n";
+            res += $"Диаметр: {Diameter} мм\n";
+            res += $"Производитель: {Manufacturer}\n";
+            res += $"Качество (Q): {CalculateQuality()}\n";
+            return res;
+        }
+
+        public static void AddCable(List<Cable> list, Cable cable)
+        {
+            list.Add(cable);
+        }
+        public static void AddCable(List<Cable> list, string type, int wireCount, double diameter, string manufacturer, double price)
+        {
+           list.Add(new Cable(type, wireCount, diameter, manufacturer, price));
+        }
+
+        public static bool RemoveCable(List<Cable> list, Cable cable)
+        {
+            bool result = list.Remove(cable);
+            return result;
+        }
+
+        public static bool RemoveCable(List<Cable> list, string type)
+        {
+            var cable = list.FirstOrDefault(c => c.Type == type);
+            if (cable != null)
+            {
+                list.Remove(cable);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ zd3_KonkovArseniy/Cable/EnhancedCable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cables
+{
+    public class EnhancedCable : Cable
+    {
+        public bool HasBraiding { get; set; }
+
+        public string Color { get; set; }
+
+        public EnhancedCable(string type, int wireCount, double diameter, string manufacturer,
+                            double pricePerMeter, bool hasBraiding, string color)
+            : base(type, wireCount, diameter, manufacturer, pricePerMeter)
+        {
+            HasBraiding = hasBraiding;
+            Color = color;
+        }
+        public double CalculateQuality()
+        {
+            double baseQuality = base.CalculateQuality();
+            return HasBraiding ? 2 * baseQuality : 0.7 * baseQuality;
+        }
+
+        public string DisplayInfo()
+        {
+            string res = "";
+            res += base.DisplayInfo();
+            res += $"Наличие оплетки: {(HasBraiding)}\n";
+            res += $"Цвет: {Color}\n";
+            res += $"Улучшенное качество (Qp): {CalculateQuality()}\n";
+            return res;
+        }
+
+        public static void AddEnhancedCable(List<EnhancedCable> list, string type, int wireCount, double diameter, string manufacturer, double price, bool hasbraiding, string color)
+        {
+            list.Add(new EnhancedCable(type, wireCount, diameter, manufacturer, price, hasbraiding, color));
+        }
+
+        public static bool RemoveEnhancedCable(List<EnhancedCable> list, EnhancedCable cable)
+        {
+            bool result = list.Remove(cable);
+            return result;
+        }
+    }
+}
